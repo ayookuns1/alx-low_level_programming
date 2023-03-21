cc3bad1d85ab5cfcc4f9c1da5bc0789df3e60841
ayookuns1 <ayookuns830@gmail.com>
2023-03-21 10:16:47 -0700

    prints alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include  "main.h"
+/**
+*print_alphabet - Program Entry point
+*
+*Return: Always 0 (success)
+*/
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+}
