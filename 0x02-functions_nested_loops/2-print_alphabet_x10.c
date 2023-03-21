ea61608402e86979dbecad9ad9b2b0dd8842e383
ayookuns1 <ayookuns830@gmail.com>
2023-03-21 11:33:53 -0700

    prints alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include  "main.h"
+/**
+*print_alphabet_x10- Program Entry point
+*
+*/
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+char j;
+for (j = 'a'; j <= 'z'; j++)
+_putchar(j);
+
+_putchar('\n');
+}
+}
