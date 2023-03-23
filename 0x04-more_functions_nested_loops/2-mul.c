70d57fe57cb3cac250f7f1b1ee003dd70c3260c0
ayookuns1 <ayookuns830@gmail.com>
2023-03-23 06:06:45 -0700

    multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+*mul - checks for uppercase
+*
+*@a: character to be multiplied
+*@b: character to be multiplied
+*Return: multiplication value
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
