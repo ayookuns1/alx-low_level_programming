1b5ff9eb79089babd987550267d25484ff2135ea
ayookuns1 <ayookuns830@gmail.com>
2023-03-27 07:45:03 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*reset_to_98 -  the function
+*@n: the integer
+*return: the value of n
+*/
+
+void reset_to_98(int *n)
+{
+
+*n = 98;
+
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+this readme file is for pointers,array. and strings
+
