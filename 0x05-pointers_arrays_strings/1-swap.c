73d31c29ceacfe2c872acba0d4dc42e13457d7de
ayookuns1 <ayookuns830@gmail.com>
2023-03-27 08:04:43 -0700

    swapping

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*swap_int -  the function
+*@a: the integer
+*@b: the second integer
+*return: the value of n
+*/
+
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
+
