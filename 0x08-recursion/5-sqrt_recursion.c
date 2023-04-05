0c11bad06dca06640a4ee6aa61c4be6131cff8d5
ayookuns1 <ayookuns830@gmail.com>
2023-04-05 15:40:29 -0700

    squareroot

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,23 +5,24 @@
  * @n: int n
  * Return: int
  */
-
 int _sqrt_recursion(int n)
 {
-if (n < 0) {
-return -1;
+	return (_sqrt(n, 1));
 }
-if (n == 0 || n == 1)
+
+/**
+ * _sqrt - _sqrt_recursion
+ * @n: integer paramtr
+ * @i: integer parameter
+ * Return: sqrt
+ */
+int _sqrt(int n, int i)
 {
-return n;
-int x ;
-x = _sqrt_recursion(n / 4) * 2;
-if ((x+1)*(x+1) <= n && (x+1)*(x+1) >= x*x)
-{
-return x+1;
-}
-else {
-return x;
-}
-}
+	if (n < 0)
+		return (-1);
+	if ((i * i) > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
 }
